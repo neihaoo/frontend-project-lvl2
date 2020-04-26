0e97c59bfabf84ee06e76e87c12c60f181cb3e81
neihaoo <neihaoo@gmail.com>
2020-04-27 00:20:12 +0300

    add agruments and option

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ const genDiff = () => {
   program
     .version('1.0.0')
     .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]', 'output format')
+    .arguments('<firstConfig> <secondConfig>')
+    .action((firstConfig, secondConfig) => {
+      console.log(`${firstConfig} | ${secondConfig}`);
+    })
     .parse(process.argv);
 
   if (!program.args.length) {
